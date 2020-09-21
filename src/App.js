1d886ceb8fd8bf7912adf887708940a01b7bf387
lgonzalez2 <lgonzalez_94@hotmail.com>
2020-09-21 16:33:43 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -30,3 +30,6 @@ class App extends Component {
 }
 
 export default App
+
+
+
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,31 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
+    const leftChars = this.props.maxChars - this.state.message.length
+
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message}/>
+        {leftChars}
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+
+
+
